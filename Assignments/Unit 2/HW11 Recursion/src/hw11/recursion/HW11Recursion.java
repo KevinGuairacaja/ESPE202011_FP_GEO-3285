60e80179d610ef27541612fd47173b07546ed6c2
KevinGuairacaja <mrkevin99@outlook.com>
2021-02-10 00:27:42 -0500

    HW11 Recursion

--- Assignments/Unit 2/HW11 Recursion/src/hw11/recursion/HW11Recursion.java
+++ Assignments/Unit 2/HW11 Recursion/src/hw11/recursion/HW11Recursion.java
@@ -6,34 +6,33 @@ public class HW11Recursion {
 
     public static void main(String[] args) {
         
+        
         System.out.println("================= Kevin Andres Guairacaja =================");
         System.out.println("Let’s solve the following mathematical permutation problem.");
         System.out.println("===========================================================");
-        System.out.println("Katterine has pulled the 4 aces out of a deck.\n"
-                + "She’s going to line them up on the table.\n"
-                + "How many different ways could she put them?\n");
-        System.out.println("Let’s think about a single combination of the 4 aces:\n"
-                + "- When we have chosen the first one, we have only 3 to choose\n"
-                + "- When we have chosen the second one, we have only 2 to choose\n"
-                + "- When we have chosen the third one, we have only 1 to choose\n"
-                + "Therefore, all possible combinations will be 4 x 3 x 2 x 1.");
-        System.out.println("\nThen: ");
-                
-        int numberX = 4;
-        int fact = 0;
-        fact = factorialNumber(numberX);
-        System.out.println("Factorial of " + numberX + " is equal to " + fact);
-        System.out.println("\nR: Katterine could place them in 24 different ways");
-    }
+        System.out.println("We want to organize a football championship. \n" +
+                            "How many possible finals we will have \n" +
+                            "Depending on the number of teams that participate");
+        Scanner e = new Scanner(System.in);
+        System.out.println("Choose how many teams will participate in your tournament: ");
+        float scanner1 = e.nextInt();
+        float numberX = scanner1;
+        float perm = 0;
+        float r = numberX - 2;
+        perm = calculateProblem((int) numberX)/calculateProblem((int) r);
+        System.out.println("\nThe possible finals you will "
+                + "have in your championship will be:\n" +perm);
+        
+        }
 
-    public static int factorialNumber(int n) {
-        if (n < 0) {
+    public static int calculateProblem(float x) {
+        if (x < 0) {
             return 0;                    
         } else {
-            if (n == 0) {
+            if (x == 0) {
                 return 1;
-            } else {  
-                return (n * factorialNumber(n - 1));
+            } else { 
+                return(int) ((x * calculateProblem(x - 1)));
             }
         }
     }
